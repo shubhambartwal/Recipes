0f98193b3ee98c5c562457b53352f6d0d84c8889
SHUBHAM BARTWAL <shubhambartwal@SHUBHAMs-MacBook-Air.local>
2024-11-08 08:43:41 +0530

    boiler-plate

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Sidebar from "./components/Sidebar"
+import HomePage from "./pages/HomePage"
+function App() {
+  return (
+    <div className="flex">
+    <Sidebar/>
+    <HomePage/>
+    </div>
+  )
+}
+export default App
