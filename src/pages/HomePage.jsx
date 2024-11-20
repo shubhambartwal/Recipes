f90d7ea93ef88eaf2936b8223532e17b2aa3fd40
shubhambartwal <shubhambartwal58@gmail.com>
2024-11-20 23:07:47 +0530

    added searching functionality using debouncing to optimise speed

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,11 @@
 import { Circle, Heart, Search } from 'lucide-react'
-import React, { useEffect, useState } from 'react'
+import React, { useDebugValue, useEffect, useState } from 'react'
 import RecipeCard from '../components/RecipeCard'
 const HomePage = () => {
   const [recipes, setRecipes] = useState([])
   const [loading, setLoading] = useState(true)
+  const [ingredient,setIngredient]=useState('chicken')
+  const debouncingIngredient=useDebounce(ingredient,500)
   const fetchRecipes = async (searchQeury) => {
     setLoading(true)
     setRecipes([])
@@ -18,21 +20,30 @@ const HomePage = () => {
       setLoading(false)
     }
   }
-  const [favourites,setFavourites]=useState([])
-  useEffect(() => { fetchRecipes("chicken") }, [])
+  function useDebounce(value,delay){
+    const [debounceValue,setDebounceValue]=useState('')
+    useEffect(()=>{
+      const handler=setTimeout(()=>{
+        setDebounceValue(value)
+      },delay)
+      return (()=>clearTimeout(handler))
+    },[value,delay])
+    return debounceValue
+  }
+  useEffect(() => { if(debouncingIngredient){fetchRecipes(debouncingIngredient)} }, [debouncingIngredient])
   return (
     <div className='bg-slate-300 p-10 flex-1 min-h-screen'>
       <div className='max-w-screen-lg mx-auto'>
         <form>
           <label className='input shadow-md flex items-center gap-2 '><Search size={"24"} />
             <input type="text" className='text-sm md:text-md grow'
-              placeholder='What do you want to eat today?' />
+              placeholder='Mention the main ingredient of your dish' onChange={((e)=>setIngredient(e.target.value))}/>
           </label>
         </form>
         <p className='font-bold text-3xl md:text-5xl mt-5 text-black'>Recommended Recipes</p>
         <p className='text-slate-500 font-semibold ml-1 my-2  text-sm  tracking-tight'>Popular choices</p>
         <div className='grid gap-3 grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
-          {!loading && recipes.map((recipe,index) => <RecipeCard  key={index} recipe={recipe} flag={false} />)}
+          {!loading && recipes?.map((recipe,index) => <RecipeCard  key={index} recipe={recipe} flag={false} />)}
           {loading && [...Array(9)].map((_, index) => (
             <div key={index }className='flex flex-col  rounded-md  gap-3  p-2  '>
               <div className='skeleton relative  w-full h-[30vh]'></div>
